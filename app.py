09c26c770212d8a143424add89e1131a2fcc48e3
Jason Baker <jasonbaker@Jasons-MacBook-Pro-3.local>
2025-04-23 08:39:07 -0400

    Increase password_hash column length and create migration

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def not_found_error(error):
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(120), nullable=False)
+    password_hash = db.Column(db.String(200), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
 
 class Customer(db.Model):
